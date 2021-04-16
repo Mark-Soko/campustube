7e79c6dfbcf861d2137fc14b647b5b174edcd721
Mark-Soko <marksoko79@gmail.com>
2021-04-17 02:55:31 +0300

    Updated production.py

--- campustube/settings/production.py
+++ campustube/settings/production.py
@@ -75,11 +75,12 @@ WSGI_APPLICATION = 'campustube.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
+
 import dj_database_url
 
 db_from_env = dj_database_url.config()
