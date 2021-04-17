ef2119f19803150a4e65c9311a216002e7c0b013
Mark-Soko <marksoko79@gmail.com>
2021-04-17 22:19:34 +0300

    Admin view

--- mpesa/admin.py
+++ mpesa/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from mpesa.models import LNMOnline
 
 # Register your models here.
-admin.site.register(LNMOnline)    
+   
+
+class myLNMOnlineAdmin(admin.modelAdmin):
+    list_display =("PhoneNumber","Amount","MpesaReceiptNumber", "TransactionDate")
+
+
+
+admin.site.register(LNMOnline,myLNMOnlineAdmin) 
